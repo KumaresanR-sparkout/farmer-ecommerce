906415617ae5b6dbfea371392c1556bba85ebb63
KumaresanR-sparkout <kumaresan@sparkouttech.com>
2024-06-13 16:50:53 +0530

    ref:un commented

--- src/index.js
+++ src/index.js
@@ -21,22 +21,19 @@ sendNotification()
 
 app.use('/api/ecom/v1', mainRouter)
 
-// if (cluster.isPrimary) {
-//     console.log(`master cpu-core of ${process.pid} is running`)
-//     for (let i = 0; i < CPUS; i++) {
-//         cluster.fork()
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`process killed at ${worker.process.pid}`)
-//         cluster.fork()
-//     })
-// }
-// else {
-//     app.listen(process.env.PORT, () => {
-//         console.log(`server started at port:${process.env.PORT}`)
-//     })
-// }
+if (cluster.isPrimary) {
+    console.log(`master cpu-core of ${process.pid} is running`)
+    for (let i = 0; i < CPUS; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`process killed at ${worker.process.pid}`)
+        cluster.fork()
+    })
+}
+else {
+    app.listen(process.env.PORT, () => {
+        console.log(`server started at port:${process.env.PORT}`)
+    })
+}
 
-app.listen(process.env.PORT, () => {
-    console.log(`server started at port:${process.env.PORT}`)
-})
